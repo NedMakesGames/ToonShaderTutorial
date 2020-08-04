d28b5dcec5ca33e3493a7cd2d5b441cd1467ee32
NedMakesGames <nedmakesgames@gmail.com>
2020-08-04 11:27:11 -0500

    Initial commit

---
+++ Assets/Shaders/Includes/Lighting Main Light Shadows.hlsl
@@ -0,0 +1,74 @@
+//    Copyright (C) 2020 Timothy Ned Atton
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// This is the Lighting.hlsl file for early parts of the tutorial
+
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, 
+        out half DistanceAtten, out half ShadowAtten) {
+#if SHADERGRAPH_PREVIEW
+    Direction = half3(0.5, 0.5, 0);
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+    #if SHADOWS_SCREEN
+        half4 clipPos = TransformWorldToHClip(WorldPos);
+        half4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+        half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+    ShadowAtten = mainLight.shadowAttenuation;
+#endif
+}
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
